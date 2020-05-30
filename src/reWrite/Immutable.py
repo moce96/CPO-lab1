95ff57e05f6f501d76b8b6758ca75db85430cb93
j <740051773@qq.com>
2020-05-30 20:05:02 +0800

    update Immutable,add the structure of <key,value>

--- src/reWrite/Immutable.py
+++ src/reWrite/Immutable.py
@@ -1,99 +1,164 @@
+from typing import TypeVar
 
+V = TypeVar(str, int, float)
 
-# 1. add a new element
-def add(self,key):
-    hash_value=key % 13
-    #If the hash value in the linked list is not already occupied by other data, the pending data (key) is placed there
-    if self.data[hash_value].data ==None:
-        self.data[hash_value].data=key
-    else:
+class Node:
+    def __init__(self, key=None,value=None):
+        self.key = key
+        self.value=value
+        self.next = None
 
-        temp=Node(key)
-        p=self.data[hash_value]
-        while p.next != None:
-            p=p.next
-        p.next=temp
+class HashMap(object):
 
-def add_from_list(self, element_list):
-    for element in element_list:
-        hash_value = element % 13
+    def __init__(self):
+        self.keyset = []
+        self.data = [Node() for _ in range(13)]
+        self.size = 13
 
-        if self.data[hash_value].data == None:
-            self.data[hash_value].data = element
+    def get(self, key: int) -> V:
+        hash_value = key % self.size
+        while True:
+            if self.keyset == None:
+                return None
+            i = 0
+            while i < 13:
+                if self.data[i].key == None:
+                    i += 1
+                    continue
+                else:
+                    p = self.data[i]
+                    while p != None:
+                        if p.key == key:
+                            return p.value
+                        p = p.next
+                    i += 1
+
+    def add(self, key, value):
+        hash_value = key % self.size
+
+        if self.data[hash_value].value == None:
+            self.data[hash_value].value = value
+            self.data[hash_value].key = key
+            self.keyset.append(key)
         else:
-
-            temp = Node(element)
+            temp = Node(key, value)
+            self.keyset.append(key)
             p = self.data[hash_value]
             while p.next != None:
                 p = p.next
             p.next = temp
 
-# 2. remove an element
-def remove(self,element):
-    hash_value=element%13
-    if self.data[hash_value].data == element:
-        self.data[hash_value].data = None
+
+    # 1. add a new element
+def add(self, key, value):
+    hash_value = key % self.size
+
+    if self.data[hash_value].value == None:
+        self.data[hash_value].value = value
+        self.data[hash_value].key = key
+        self.keyset.append(key)
     else:
-        p=self.data[hash_value]
-        pre = None
-        while p.next!=None:
-            if p != None and p.data!=element:
-                pre = p
-                p = p.next
-            if p == None:
-                return 'Delete Error'
-            else:
-                pre.next = p.next
+        temp = Node(key, value)
+        self.keyset.append(key)
+        p = self.data[hash_value]
+        while p.next != None:
+            p = p.next
+        p.next = temp
 
-
-# 3. size
-def getSize(self):
-    sum = 0
-
+    # 2. remove an element
+def remove(self,key):
     i = 0
-    while i < 13:
-        if self.data[i].data == None:
-            i += 1
+    while i < self.size:
+        if self.data[i].key==None:
+            i+=1
             continue
         else:
             p = self.data[i]
-            while p != None:
-                sum += 1
-                p = p.next
-            i += 1
+            if p.key==key:
+                if p.next==None:
+                    p.key=None
+                    p.value=None
+                else:
+                    temp=p.next
+                    p.key=temp.key
+                    p.value=temp.value
+                    p.next=temp.next
+                remove_keyset(self,key)
+                return self
+            else:
+                while p != None:
+                    if p.key == key:
+                        temp = p.next
+                        p.next=temp.next
+                        remove_keyset(self, key)
+                        return self
+                    else:
+                        p = p.next
+        i += 1
+    return self
 
-    return sum
 
+
+def remove_keyset(hashmap,key):
+    for i,k in enumerate(hashmap.keyset):
+        if key==k:
+            hashmap.keyset.remove(i)
+            break
+
+    # 3. size
+def getSize(self):
+        sum=0
+
+        i = 0
+        while i < 13:
+            if self.data[i].value == None:
+                i += 1
+                continue
+            else:
+                p = self.data[i]
+                while p != None:
+                    sum+=1
+                    p = p.next
+                i += 1
+
+        return sum
 
     # 4. conversion from and to python lists
 def to_list(self):
     list = []
-    i = 0
-    while i < 13:
-        if self.data[i].data == None:
-            i += 1
-            continue
-        else:
-            p = self.data[i]
-            while p != None:
-                list.append(p.data)
-                p = p.next
-
-            i += 1
-
+    for i in self.keyset:
+        list.append(self.get(i))
     return list
 
 
 def from_list(self, list):
-    for i in list:
-        self.add(i)
+    for k, v in enumerate(list):
+        self.add(k, v)
     return self
 
+
+def get(self, key: int) -> V:
+    hash_value = key % self.size
+    while True:
+        if self.keyset == None:
+            return None
+        i = 0
+        while i < 13:
+            if self.data[i].key == None:
+                i += 1
+                continue
+            else:
+                p = self.data[i]
+                while p != None:
+                    if p.key == key:
+                        return p.value
+                    p = p.next
+                i += 1
 
 # 5. ﬁnd element by speciﬁc predicate
 def find_iseven(self):
     mylist = to_list(self)
-    mylist1 = []
+    mylist1=[]
     for k in range(len(mylist)):
         if mylist[k] % 2 == 0:
             mylist1.append(mylist[k])
@@ -112,46 +177,56 @@ def filter_iseven(self):
 # 7. map structure by speciﬁc function
 def a_map(self, f):
     i = 0
-    list=self.to_list()
+    list=to_list(self)
     for v in list:
         list[i] = f(v)
         i += 1
     return list
-
 
 # 8. reduce:process structure elements to build a return value by speciﬁc functions
 def reduce(self, f, initial_state):
     state = initial_state
     i = 0
     while i < 13:
-        if self.data[i].data == None:
+        if self.data[i].value == None:
             i += 1
             continue
         else:
             p = self.data[i]
-            state = f(state, p.data)
+            state = f(state, p.value)
             while p.next != None:
                 p = p.next
-                state = f(state, p.data)
+                state = f(state, p.value)
         i += 1
     return state
 
 # 9. mempty and mconcat
 def mempty(self):
-    return list([])
+    i = 0
+    while i < 13:
+        if self.data[i].value == None:
+            i += 1
+            continue
+        else:
+            p = self.data[i]
+            p.key = None
+            p.value = None
+            p.next = None
+        i += 1
+    self.keyset = []
+    return self
 
 
 def mconcat(a, b):
     if a is None:
         return b
-    # if b is None:
-    #     return a
-    if b is not None:
-        list = to_list(b)
-        for i in list:
-            add(a,i)
-    return a
-
+    else:
+        if b is None:
+            return a
+        else:
+            list = to_list(b)
+            for i,v in enumerate(list):
+                add(a,i,v)
 
 
 # 10. iterator
@@ -173,7 +248,4 @@ def iterator(n):
     return get_next
 
 
-class Node:
-    def __init__(self, data=None):
-        self.data = data
-        self.next = None
+
--- src/reWrite/Immutable_test.py
+++ src/reWrite/Immutable_test.py
@@ -3,73 +3,93 @@ import unittest
 from hypothesis import given
 import hypothesis.strategies as st
 
-
-from src.reWrite.Immutable import add, to_list, remove, getSize, find_iseven, filter_iseven, a_map, reduce, \
-    add_from_list, mconcat, mempty, iterator, from_list
-from src.reWrite.Mutable import HashMap
+from src.reWrite.Immutable import HashMap, add, to_list, remove, getSize, from_list, find_iseven, filter_iseven, a_map, \
+    reduce, mempty, mconcat, iterator
 
 
 class TestImmutableList(unittest.TestCase):
     # 1. add a new element
     def test_add(self):
         hash = HashMap()
-        add(hash, 1)
+        add(hash, 1,1)
         self.assertEqual(to_list(hash), [1])
-        add(hash, 2)
-        self.assertEqual(to_list(hash), [1,2])
+        add(hash, 2,2)
+        self.assertEqual(to_list(hash), [1, 2])
 
     # 2. remove an element
     def test_remove(self):
         hash = HashMap()
-        add(hash, 0)
-        add(hash, 1)
-        add(hash, 2)
-        add(hash, 3)
-        remove(hash,3)
-        self.assertEqual(to_list(hash), [0, 1, 2])
-        remove(hash, 2)
-        self.assertEqual(to_list(hash), [0, 1])
+        add(hash, 0,0)
+        add(hash, 1,1)
+        add(hash, 2,2)
+        add(hash, 3,3)
+        r_a=remove(hash,0)
+        self.assertEqual(to_list(r_a), [1, 2,3])
+        # remove(hash, 2)
+        # self.assertEqual(to_list(hash), [0, 1])
     #
     # 3. size
     def test_getSize(self):
         hash = HashMap()
-        add(hash, 0)
-        add(hash, 1)
-        add(hash, 2)
-        add(hash, 3)
+        add(hash, 0,0)
+        add(hash, 1,1)
+        add(hash, 2,2)
+        add(hash, 3,3)
         self.assertEqual(getSize(hash), 4)
         remove(hash,3)
         self.assertEqual(getSize(hash), 3)
-    #
+
+    # 4. conversion from and to python lists
+    def test_from_list(self):
+        test_data = [
+            [],
+            [1],
+            [1, 2]
+        ]
+        for e in test_data:
+            hash = HashMap()
+            from_list(hash,e)
+            self.assertEqual(to_list(hash), e)
+
+    def test_to_list(self):
+        hash = HashMap()
+        add(hash, 0,0)
+        add(hash, 1,1)
+        add(hash, 2,2)
+        add(hash, 3,3)
+        self.assertEqual(to_list(hash), [0, 1, 2, 3])
+        hash2 = HashMap()
+        self.assertEqual(to_list(hash2), [])
+
     # 5. ﬁnd element by speciﬁc predicate
     def test_find_iseven(self):
         hash = HashMap()
-        add(hash, 1)
-        add(hash, 2)
+        add(hash, 1,1)
+        add(hash, 2,2)
         self.assertEqual(find_iseven(hash), [2])
-        add(hash, 3)
-        add(hash, 4)
+        add(hash, 3,3)
+        add(hash, 4,4)
         self.assertEqual(find_iseven(hash), [2, 4])
-    #
+
     # 6. ﬁlter data structure by speciﬁc predicate
     def test_filter_iseven(self):
         hash = HashMap()
-        add(hash, 1)
-        add(hash, 2)
+        add(hash, 1,1)
+        add(hash, 2,2)
         self.assertEqual(filter_iseven(hash), [1])
-        add(hash, 3)
-        add(hash, 4)
+        add(hash, 3,3)
+        add(hash, 4,4)
         self.assertEqual(filter_iseven(hash), [1, 3])
-    #
+
     # 7. map structure by speciﬁc function
     def test_a_map(self):
         hash = HashMap()
         self.assertEqual(a_map(hash,str), [])
-        add(hash, 1)
-        add(hash, 2)
+        add(hash, 1,1)
+        add(hash, 2,2)
         self.assertEqual(a_map(hash,str), ['1', '2'])
-        hash.add(3)
-        hash.add(4)
+        add(hash, 3,3)
+        add(hash, 4,4)
         self.assertEqual(a_map(hash,lambda x: x + 1), [2, 3, 4, 5])
 
     # 8. reduce – process structure elements to build a return value by speciﬁc functions
@@ -85,18 +105,20 @@ class TestImmutableList(unittest.TestCase):
 
     # 9. mempty and mconcat
     def test_mempty(self):
-        self.assertEqual(mempty([]), [])
-        self.assertEqual(mempty([1, 2, 3, 4]), [])
+        hash = HashMap()
+        add(hash, 1,1)
+        add(hash, 2,2)
+        self.assertEqual(to_list(mempty(hash)), [])
 
     def test_mconcat(self):
         hash = HashMap()
         hash1=HashMap()
+        mconcat(hash, hash1)
+        self.assertEqual(to_list(hash), [])
+        add(hash, 1,1)
+        add(hash1, 2,2)
         mconcat(hash,hash1)
-        self.assertEqual(hash.to_list(), [])
-        add(hash1,1)
-        add(hash1,2)
-        mconcat(hash,hash1)
-        self.assertEqual(hash.to_list(), [1, 2])
+        self.assertEqual(to_list(hash), [1, 2])
 
 
     @given(st.lists(st.integers()))
@@ -105,8 +127,7 @@ class TestImmutableList(unittest.TestCase):
         from_list(hash, a)
         b = to_list(hash)
         self.assertEqual(a.sort(), b.sort())
-    #
-    # #
+
     @given(st.lists(st.integers()))
     def test_python_len_and_list_size_equality(self, lst):
         hash = HashMap()
